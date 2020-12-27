33abf1fd8853c196b3964737f766d6e1de48e274
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2020-12-27 20:11:17 +0200

    feat(store): add more constructors for VarStruct

--- src/store/varStruct.h
+++ src/store/varStruct.h
@@ -8,10 +8,14 @@
 
 typedef struct VarStruct
 {
+    VarStruct()
+        : type(VAR_TYPE_FLOAT), vFloat(0.0f), vInt(0) {}
     VarStruct(long int _value)
         : type(VAR_TYPE_LONG), vFloat((float)_value), vInt(_value) {}
     VarStruct(float _value)
-        : type(VAR_TYPE_LONG), vFloat(_value), vInt(round(_value)) {}
+        : type(VAR_TYPE_FLOAT), vFloat(_value), vInt(round(_value)) {}
+    VarStruct(VarStruct *_value)
+        : type(_value->type), vFloat(_value->vFloat), vInt(_value->vInt) {}
 
     char type;
     volatile long int vInt;
